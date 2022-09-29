a4c45a602416d5c6d2302b4a3e9bfd4e07dca208
Kshetra Hegde <kshetrahegde9@gmail.com>
2022-09-29 14:11:35 -0400

    First commit

---
+++ HumanResource/src/model/EmployeeHistory.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author kshetrahegde
+ */
+public class EmployeeHistory {
+    
+    private ArrayList<EmployeeInfo> history;
+    
+    public EmployeeHistory(){
+        
+        this.history = new ArrayList<EmployeeInfo>();    
+}
+
+    public ArrayList<EmployeeInfo> getHistory() {
+        return history;
+    }
+
+    public void setHistory(ArrayList<EmployeeInfo> history) {
+        this.history = history;
+    }
+    
+    public EmployeeInfo addNewEmployee(){
+        EmployeeInfo addEmployee = new EmployeeInfo();
+        history.add(addEmployee);
+        return addEmployee;
+    }
+    
+}
---
+++ HumanResource/src/model/EmployeeInfo.java
@@ -0,0 +1,113 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.Date;
+
+
+
+/**
+ *
+ * @author kshetrahegde
+ */
+public class EmployeeInfo {
+    
+    private String name;
+    private int id;
+    private int age;
+    private String gender;
+    private String startdate;
+    private String level;
+    private String teaminfo;
+    private String position;
+    private int phone;
+    private String emailaddr;
+    // Documents/JavaAssignments/HumanResource/Article1.png
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getStartdate() {
+        return startdate;
+    }
+
+    public void setStartdate(String startdate) {
+        this.startdate = startdate;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getTeaminfo() {
+        return teaminfo;
+    }
+
+    public void setTeaminfo(String teaminfo) {
+        this.teaminfo = teaminfo;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getEmailaddr() {
+        return emailaddr;
+    }
+
+    public void setEmailaddr(String emailaddr) {
+        this.emailaddr = emailaddr;
+    }
+            
+    
+    
+    
+}
